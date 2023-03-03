bb13701da61a35e2e9317e8d4535e8b827da6864
elmarputz <github@elp.name>
2023-03-03 15:17:44 +0100

    view test

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $books =  [
+        'Herr der Ringe 1',
+        'Herr der Ringe 2',
+        'Herr der Ringe 3',
+    ];
+
+    return view('welcome', [
+        'books' => $books
+    ]);
 });
